64a61ff4d6852e5fc98ec91b28a09f6fd363fe1a
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-12 23:31:55 +0200

    Create simple line chart

--- Section1/script.js
+++ Section1/script.js
@@ -1,1 +1,17 @@
-'use strict'
+'use strict';
+
+const ctx = document.getElementById('canvas').getContext('2d');
+
+const config = {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May'],
+    datasets: [
+      {
+        data: [1, 4, 1, 5, 9],
+      },
+    ],
+  },
+};
+
+new Chart(ctx, config);
