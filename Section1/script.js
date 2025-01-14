20dc6a4c7b89e4ce9bc0bc7d09736b1bfbe5284b
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-14 16:48:16 +0200

    Revert "Change chart type to 'bar'"
    This reverts commit edf70756ff33b6db84a4afbd324ae0045a367607.

--- Section1/script.js
+++ Section1/script.js
@@ -3,7 +3,7 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
 const config = {
-  type: 'bar',
+  type: 'line',
   data: {
     labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
