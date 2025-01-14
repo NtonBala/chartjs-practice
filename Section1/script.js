edf70756ff33b6db84a4afbd324ae0045a367607
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-14 16:41:46 +0200

    Change chart type to 'bar'

--- Section1/script.js
+++ Section1/script.js
@@ -3,7 +3,7 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
 const config = {
-  type: 'line',
+  type: 'bar',
   data: {
     labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
