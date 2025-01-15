28a3b403096eaf2bd7ef0d26f73f4b7c21bae5e5
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-15 18:43:57 +0200

    Create bubble chart

--- Section1/script.js
+++ Section1/script.js
@@ -3,17 +3,17 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
 const config = {
-  type: 'scatter',
+  type: 'bubble',
   data: {
     datasets: [
       {
         data: [
-          { x: 20, y: 30 },
-          { x: 45, y: 12 },
-          { x: 24, y: 33 },
-          { x: 15, y: 17 },
-          { x: 30, y: 24 },
-          { x: 37, y: 15 },
+          { x: 20, y: 30, r: 48 },
+          { x: 45, y: 12, r: 22 },
+          { x: 24, y: 33, r: 40 },
+          { x: 15, y: 17, r: 16 },
+          { x: 30, y: 24, r: 100 },
+          { x: 37, y: 15, r: 80 },
         ],
         pointStyle: 'circle',
         pintRadius: 2,
