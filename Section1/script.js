9a76ec53de98d4945731f46f8902644ee610b60b
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-16 18:59:35 +0200

    Reset to line chart

--- Section1/script.js
+++ Section1/script.js
@@ -3,20 +3,12 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 
 const config = {
-  type: 'bubble',
+  type: 'line',
   data: {
+    labels: ['January', 'February', 'March', 'April', 'May'],
     datasets: [
       {
-        data: [
-          { x: 20, y: 30, r: 48 },
-          { x: 45, y: 12, r: 22 },
-          { x: 24, y: 33, r: 40 },
-          { x: 15, y: 17, r: 16 },
-          { x: 30, y: 24, r: 100 },
-          { x: 37, y: 15, r: 80 },
-        ],
-        pointStyle: 'circle',
-        pintRadius: 2,
+        data: [1, 4, 1, 5, 9],
       },
     ],
   },
