42353c0827f8efb6148fea1300d1c993a696fc11
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-19 14:48:51 +0200

    Create simple Seasons chart of type 'doughnut'

--- Section2/script.js
+++ Section2/script.js
@@ -1,1 +1,17 @@
-'use strict'
+'use strict';
+
+const canvas = document.getElementById('myChart');
+const ctx = canvas.getContext('2d');
+
+const graph = {
+  charting() {
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        datasets: [{ data: Array(4).fill(1), backgroundColor: 'green', label: ['Winter', 'Spring', 'Summer', 'Autumn'] }],
+      },
+    });
+  },
+};
+
+graph.charting();
