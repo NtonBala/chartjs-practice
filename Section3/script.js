0d4d7f05f91103ecf53d063496ff5d658f798db6
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-27 14:01:16 +0200

    [polarArea] add comments

--- Section3/script.js
+++ Section3/script.js
@@ -8,6 +8,8 @@ ctx.canvas.parentNode.style.width = '100%';
 
 const graph = {
   piChart: null,
+  // * Array of numbers each showing how often (%) a digit from 0 to 9 occur in the number of Pi.
+  // * Digit from 0 to 9 are represented by array indexes.
   myData: [0, 20, 10, 10, 10, 30, 10, 0, 0, 10],
   myLabels: [...Array(10).keys()],
 
