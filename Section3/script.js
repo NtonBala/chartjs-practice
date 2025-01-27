30c856ca74638301be9aa0fadd9fd4f12de12505
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-27 12:59:34 +0200

    [polarArea] prepare graph template

--- Section3/script.js
+++ Section3/script.js
@@ -1,1 +1,30 @@
-'use strict'
+'use strict';
+
+const canvas = document.getElementById('myChart');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.parentNode.style.height = '92%';
+ctx.canvas.parentNode.style.width = '100%';
+
+const graph = {
+  piChart: null,
+  myData: [0, 20, 10, 10, 10, 30, 10, 0, 0, 10],
+  myLabels: [...Array(10).keys()],
+
+  charting() {
+    graph.piChart = new Chart(ctx, {
+      type: 'polarArea',
+      data: {
+        datasets: [{ data: graph.myData }],
+        labels: graph.myLabels,
+      },
+      options: {
+        maintainAspectRatio: false,
+      },
+    });
+  },
+};
+
+window.addEventListener('load', function () {
+  graph.charting();
+});
