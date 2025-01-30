e1435bbccd55810341dc9ed8adf974cb3f8726e5
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-30 12:45:04 +0200

    [polarArea] add mechanism for updating dynamic chart

--- Section3/script.js
+++ Section3/script.js
@@ -18,6 +18,16 @@ const graph = {
   tenDigits: [],
   // * A fraction to display the limit digit to be analyzed.
   limit: 500 / 50,
+  // * Frequency distribution - counts the number of occurrences of corresponding digits.
+  distribution: Array(10).fill(0),
+  // * Percentage of occurrences for each digit from 0 to 9 (section labels value).
+  percent: Array(10).fill(0),
+  // * Interval used to dynamically change the charts.
+  timerId: null,
+  // * Counts the number of new charts. One chart corresponds to ten decimal places analyzed.
+  step: 0,
+  // * Controls the time interval of the chart change in ms.
+  speed: 1000,
 
   charting() {
     graph.piChart = new Chart(ctx, {
@@ -40,6 +50,12 @@ const graph = {
               weight: 'bold',
             },
           },
+          subtitle: {
+            display: true,
+            text: 'step: 0',
+            color: 'yellow',
+            font: { size: 44 },
+          },
           legend: { display: false },
           datalabels: {
             formatter: (value, context) => `${context.dataIndex}: ${value.toFixed(1)}%`,
@@ -51,6 +67,40 @@ const graph = {
         },
       },
     });
+  },
+
+  updatePiChart(chart, info) {
+    const { step, limit, timerId, piChart, percent, speed } = this;
+
+    if (step <= limit) {
+      graph.updateData();
+      chart.data.datasets.splice(0, 1, { data: info });
+      chart.options.plugins.subtitle.text = step;
+
+      chart.update();
+      // ! As we start below new timers with new arguments that leads to unpredictable results. To avoid this we clear interval first.
+      clearInterval(timerId);
+      this.timerId = setInterval(() => graph.updatePiChart(piChart, percent), speed);
+    } else {
+      clearInterval(this.timerId);
+    }
+  },
+
+  // * Updates the data (percent array) to build a new chart.
+  updateData() {
+    const { tenDigits, distribution, percent, step } = this;
+
+    for (let digit of tenDigits[step]) {
+      distribution[digit]++;
+    }
+
+    const result = distribution.reduce((sum, elem) => sum + elem, 0);
+
+    for (let i = 0; i < 10; i++) {
+      percent[i] = (100 * distribution[i]) / result;
+    }
+
+    this.step++;
   },
 
   // * Reads data file (e.g. data.txt) and builds tenDigits array.
@@ -66,7 +116,7 @@ const graph = {
         const allData = reader.result.split('\r\n').slice(0, graph.limit);
         graph.tenDigits = allData.flatMap((data) => data.split(' ').slice(0, 5));
 
-        console.log(graph.tenDigits);
+        graph.updatePiChart(graph.piChart, graph.percent);
       };
     } else {
       return alert(`${file.name} is not a valid text file`);
